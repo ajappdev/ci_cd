b715311c0b4c4ecea648f74bb7d50b979fd94394
AAJ <distripha@obi-conseil.com>
2024-06-24 11:22:29 +0100

    sss

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ def print_phrase_words(phrase: str):
     for word in phrase.split():
         print(word)
         
-print_phrase_words("Today I'm very happy because I'm moving towards 2000 mo per month")
+print_phrase_words("Today I'm very happy because I'm moving towards $2000 per month")
